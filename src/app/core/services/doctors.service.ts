45477d35ce2fb182d365b435a434320d65505e2e
Mosiur Rahman <connect.mosiur@gmail.com>
2026-02-21 17:16:05 +0600

    Prepared doctors list with mock data and with basic search

--- src/app/core/services/doctors.service.ts
+++ src/app/core/services/doctors.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Doctor } from '../models/doctors.model';
 
-@Injectable ({
-    providedIn: 'root'
+@Injectable({
+  providedIn: 'root',
 })
-
 export class DoctorService {
-    private mockDoctors: Doctor[] = [ // ekta mock data insert korsi development er jonno
-        {
-                  _id: '1',
+  private mockDoctors: Doctor[] = [
+    // ekta mock data insert korsi development er jonno
+    {
+      _id: '1',
       name: 'Dr. Ahmed Rahman',
       slug: 'dr-ahmed-rahman',
       specialty: 'Cardiologist',
@@ -18,36 +18,42 @@ export class DoctorService {
       experience: 12,
       fee: 800,
       verified: true,
-      image: ''
-        }
-    ];
-  
-  getDoctors(filters?: { // getDoctors method create korsi eta doctor list theke all doctor ene dekhabe, jodi filter parameters pass kore thahole filtered information dekhabe
+      image: '',
+    },
+  ];
+
+  getDoctors(filters?: {
+    // getDoctors method create korsi eta doctor list theke all doctor ene dekhabe, jodi filter parameters pass kore thahole filtered information dekhabe
     specialty?: string;
     city?: string;
     hospital?: string;
   }): Observable<Doctor[]> {
     let filteredDoctors = this.mockDoctors;
-
+    console.log(filters);
     if (filters?.specialty) {
-      filteredDoctors = filteredDoctors.filter(doctor => doctor.specialty.toLowerCase() === filters.specialty?.toLowerCase());
+      filteredDoctors = filteredDoctors.filter(
+        (doctor) =>
+          doctor.specialty.toLowerCase() === filters.specialty?.toLowerCase(),
+      );
     }
 
     if (filters?.city) {
-      filteredDoctors = filteredDoctors.filter(doctor => doctor.city === filters.city?.toLowerCase());
+      filteredDoctors = filteredDoctors.filter(
+        (doctor) => doctor.city === filters.city!,
+      );
     }
 
     if (filters?.hospital) {
-      filteredDoctors = filteredDoctors.filter(doctor => doctor.hospital.toLowerCase() === filters.hospital?.toLowerCase());
+      filteredDoctors = filteredDoctors.filter(
+        (doctor) =>
+          doctor.hospital.toLowerCase() === filters.hospital?.toLowerCase(),
+      );
     }
 
     return of(filteredDoctors); // of operator use korsi jate observable return kore, jate subscribe kora jay component theke
   }
 
   getDoctorsBySlug(slug: string): Observable<Doctor | undefined> {
-    return of(this.mockDoctors.find(doctor => doctor.slug === slug));
+    return of(this.mockDoctors.find((doctor) => doctor.slug === slug));
   }
-
-
-
 } // DoctorService end parenthesis
--- src/app/features/doctors/doctor-list/doctor-list.component.ts
+++ src/app/features/doctors/doctor-list/doctor-list.component.ts
@@ -5,40 +5,42 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-
 @Component({
   selector: 'app-doctor-list',
   standalone: true,
-  imports: [RouterModule,CommonModule,FormsModule],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './doctor-list.component.html',
-  styleUrl: './doctor-list.component.css'
+  styleUrl: './doctor-list.component.css',
 })
-
-
 export class DoctorListComponent implements OnInit {
-    doctors: Doctor[] = [];
-    city: string = '';
-    specialty: string = ''; 
-    hospital: string = '';
-    
-    constructor(private doctorService: DoctorService) { }
+  doctors: Doctor[] = [];
+  city: string = '';
+  specialty: string = '';
+  hospital: string = '';
 
-    loadDoctors() { // ekhane ekta function create korsi
-       this.doctorService.getDoctors({ //function er moddhe doctor service call kore getDoctor method call korsi
-            city: this.city,
-            specialty: this.specialty,  // GetDoctor er moddhe j optional parameters chilo ogula pass korsi
-            hospital: this.hospital
-        }).subscribe(doctors => {  // observable er sathe subscribe korsi jate response ashe taropor doctor variable er sathe assign kora jay
-            this.doctors = doctors; // doctor variable er sathe assign korsi
-        });
-    }
+  constructor(private doctorService: DoctorService) {}
 
-    ngOnInit(): void {
-        this.loadDoctors(); //loadDoctor fuiction ta onInit e call korsi jate page load hower asathe sathe info ta dekhay
+  loadDoctors() {
+    // ekhane ekta function create korsi
+    this.doctorService
+      .getDoctors({
+        //function er moddhe doctor service call kore getDoctor method call korsi
+        city: this.city,
+        specialty: this.specialty, // GetDoctor er moddhe j optional parameters chilo ogula pass korsi
+        hospital: this.hospital,
+      })
+      .subscribe((doctors) => {
+        // observable er sathe subscribe korsi jate response ashe taropor doctor variable er sathe assign kora jay
+        this.doctors = doctors; // doctor variable er sathe assign korsi
+      });
+  }
 
-      }
+  ngOnInit(): void {
+    this.loadDoctors(); //loadDoctor fuiction ta onInit e call korsi jate page load hower asathe sathe info ta dekhay
+  }
 
-      onFilterChange(): void { // filter change hole abar doctor load kora hobe
-        this.loadDoctors(); //kivabe kaj kore ekhono uncertain, but filter change hole abar doctor load kora hobe
-      }
+  onFilterChange(): void {
+    // filter change hole abar doctor load kora hobe
+    this.loadDoctors(); //kivabe kaj kore ekhono uncertain, but filter change hole abar doctor load kora hobe
+  }
 } // Component end parenthesis
